b3372e5539c1fa7f4063746b542dd1d154465799
Robert Watson <spartan83@gmail.com>
2017-12-10 07:13:50 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,13 @@
+# def reverse_each_word (array)
+#   array = array.split
+#   new_array = []
+#   array.each do |word|
+#     new_array << "#{word.reverse} "
+#   end
+#   new_array = new_array.join
+#   new_array.strip
+# end
+
 def reverse_each_word (array)
   array = array.split
   new_array = []
